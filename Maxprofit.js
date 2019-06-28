057c85c3886ddf7ee5c912256ef129f34e4ec833
sritharanpalani <irstharan04@gmail.com>
2019-06-28 21:46:18 +0530

    combination of properties

--- Maxprofit.js
+++ Maxprofit.js
@@ -6,30 +6,52 @@ const readline = require('readline').createInterface({
   })
   
 readline.question(`Enter time period: `, (givenTime) => {
-        var property = [
-            { time : 5, amount : 1500, name: 'Theatre' },
-            { time : 4, amount : 1000, name: 'Mall' },
-            { time : 10, amount : 3000, name: 'CommercialPlace' },
-        ]
-    
-    
+
+    const  profitMaker = (property) => {
+
         var result = [];
-        
+
         for (const item of property) {
+
             let periodTekenToBuild = Math.trunc(givenTime / item.time);
+            let remainingTime = givenTime % item.time;
+            var solutionT = 0
+            
             var maximumAmount = 0;
-        
+
             for (let index = 1; index <= periodTekenToBuild; index++) {
+                
                 let amount = (givenTime - (index * item.time)) * item.amount;
                 maximumAmount += amount
             }
-        
-            result.push({name: item.name, maximumProfit: maximumAmount, solution: periodTekenToBuild });
+
+            
+            if(givenTime > 9 && remainingTime > 6) {
+                let Theatre = property[0];
+                let amount = (remainingTime - Theatre.time) * Theatre.amount;
+                maximumAmount += amount;
+                solutionT = 1
+            }
+            
+            result.push({
+                name: solutionT === 1 ? `${item.name}, Theatre` : item.name, 
+                maximumProfit: maximumAmount, 
+                solution: solutionT === 1 ? `${periodTekenToBuild}, ${solutionT}` : periodTekenToBuild,
+            });
         } 
 
         let profit = result.sort((a,b) => {
             return b.maximumProfit - a.maximumProfit;
         })
-        console.log('this is final result ====', profit);
+        console.log('Final result ====', profit);
+
+    }
+
+    profitMaker( [
+        { time : 5, amount : 1500, name: 'Theatre' },
+        { time : 4, amount : 1000, name: 'Mall' },
+        { time : 10, amount : 3000, name: 'CommercialPlace' },
+    ]);
     readline.close()
 })
+
