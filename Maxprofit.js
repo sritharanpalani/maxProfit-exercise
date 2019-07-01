0062b36d487979c345c5cbe68410f66c079e838e
sritharanpalani <irstharan04@gmail.com>
2019-07-01 11:50:45 +0530

    combination of items check

--- Maxprofit.js
+++ Maxprofit.js
@@ -10,41 +10,79 @@ readline.question(`Enter time period: `, (givenTime) => {
     const  profitMaker = (property) => {
 
         var result = [];
-
         for (const item of property) {
 
             let periodTekenToBuild = Math.trunc(givenTime / item.time);
             let remainingTime = givenTime % item.time;
             var solutionT = 0
+            var solutionM = 0;
             
             var maximumAmount = 0;
 
-            for (let index = 1; index <= periodTekenToBuild; index++) {
-                
-                let amount = (givenTime - (index * item.time)) * item.amount;
-                maximumAmount += amount
-            }
+            if(remainingTime === 0) {
+                let remainPeriod = periodTekenToBuild - 1
+                maximumAmount += yeatCalculator((remainPeriod), item)
+                let balancePeriod = givenTime - ((remainPeriod) * item.time)
 
-            
-            if(givenTime > 9 && remainingTime > 6) {
-                let Theatre = property[0];
-                let amount = (remainingTime - Theatre.time) * Theatre.amount;
-                maximumAmount += amount;
-                solutionT = 1
+                if(balancePeriod > 4 && balancePeriod < 6) {
+                    let amount = counter(property[1], balancePeriod);
+                    maximumAmount += amount
+                    solutionM += 1
+                } else if (balancePeriod > 6 ) {
+                    let amount = counter(property[0], balancePeriod)
+                    maximumAmount += amount
+                    solutionT += 1
+
+                    if(balancePeriod === 10) {
+                        let amount = counter(property[1], (balancePeriod-5))
+                        maximumAmount += amount
+                        solutionM += 1
+                    }
+                }
+                periodTekenToBuild = remainPeriod
+            } else {
+                maximumAmount = yeatCalculator(periodTekenToBuild, item)
+
+                if(remainingTime === 5) {
+                    let amount = counter(property[1], remainingTime);
+                    maximumAmount += amount;
+                    solutionM += 1;
+                } else if (givenTime > 9 && remainingTime > 6) {
+                    let amount  = counter(property[0], remainingTime)
+                    maximumAmount += amount;
+                    solutionT += 1
+                } else {
+
+                }
             }
             
             result.push({
-                name: solutionT === 1 ? `${item.name}, Theatre` : item.name, 
+                name: `${item.name} ${solutionT !== 0 ? ', Theatre': ''} ${solutionM !== 0 ? ', Mall' : ''}`, 
                 maximumProfit: maximumAmount, 
-                solution: solutionT === 1 ? `${periodTekenToBuild}, ${solutionT}` : periodTekenToBuild,
+                solution: `${periodTekenToBuild} ${solutionT !== 0 ? solutionT: ''} ${solutionM !== 0 ? solutionM : ''}`,
             });
         } 
 
-        let profit = result.sort((a,b) => {
-            return b.maximumProfit - a.maximumProfit;
-        })
-        console.log('Final result ====', profit);
 
+        let maxVal = result.reduce((max,p) =>  p.maximumProfit > max ? p.maximumProfit : max, result[0].maximumProfit)
+
+        let sortedProfit = result.filter((a) => a.maximumProfit === maxVal);
+
+        console.log('Final result', sortedProfit);
+
+    }
+
+    const counter = (property, remainingTime) => {
+        return amount = (remainingTime - property.time) * property.amount;
+    }
+
+    const yeatCalculator = (period, item) => {
+        let amount = 0
+        for (let index = 1; index <= period; index++) {
+              amount += (givenTime -(index * item.time)) * item.amount;
+        }
+
+        return amount;
     }
 
     profitMaker( [
@@ -53,5 +91,6 @@ readline.question(`Enter time period: `, (givenTime) => {
         { time : 10, amount : 3000, name: 'CommercialPlace' },
     ]);
     readline.close()
+
 })
 
